9d34459be18e6ff4ebc7ffb913d7a51808425a45
Zachary Curry <zachcurrycodes@gmail.com>
2020-03-23 15:42:56 -0500

    Walking Skeleton Completed

--- SS/SS-SPA/src/app/value/value.component.ts
+++ SS/SS-SPA/src/app/value/value.component.ts
@@ -7,11 +7,21 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./value.component.css']
 })
 export class ValueComponent implements OnInit {
+  values: any;
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getValues();
+  }
 
   getValues() {
-    this.http.get('http://localhost:5000/api/values');
+    this.http.get('http://localhost:5000/api/values').subscribe(
+      response => {
+        this.values = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 }
--- SS/SS.API/Startup.cs
+++ SS/SS.API/Startup.cs
@@ -30,6 +30,7 @@ namespace SS.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,8 @@ namespace SS.API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
